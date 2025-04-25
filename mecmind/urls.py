a7ce227e6e9593a5be85b2c07a3adf1ff00c4421
Diego Martines <diegosmmartinesdev@gmail.com>
2025-04-25 17:48:45 -0300

    Adicionando a url de admin.

--- mecmind/urls.py
+++ mecmind/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from django.contrib import admin
 
 app_name = 'mecmind_app'
 
 urlpatterns = [
     path('', include('mecmind_app.urls')),
+    path('admin/', admin.site.urls),
 ]
